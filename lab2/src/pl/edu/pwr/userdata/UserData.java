db79532bcf888fecf1f3fa2411e2c262c7fb91e5
WNowak <bulion@zoho.com>
2022-03-10 21:46:57 +0100

    add UserDataFileReader

--- lab2/src/pl/edu/pwr/userdata/UserData.java
+++ lab2/src/pl/edu/pwr/userdata/UserData.java
@@ -2,24 +2,23 @@ package pl.edu.pwr.userdata;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
 
 public class UserData {
-    private final String name;
+    private final String firstName;
     private final String lastName;
     private final int age;
     private final ImageIcon profilePic;
 
-    public UserData(String name, String lastName, int age, String imagePath) {
-        this.name = name;
+    public UserData(String firstName, String lastName, int age, String imagePath) {
+        this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
         ImageIcon icon = new ImageIcon(imagePath);
         this.profilePic = new ImageIcon(icon.getImage().getScaledInstance(150, 150, Image.SCALE_SMOOTH));
     }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
 
     public String getLastName() {
