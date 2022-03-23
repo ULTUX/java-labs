6b697488e1873c0813cb880432156073108e6314
WNowak <bulion@zoho.com>
2022-03-23 22:35:07 +0100

    Fix spelling errors

--- lab3/src/main/java/pl/edu/pwr/quizapp/lang/LocaleClassBundle_pl_PL.java
+++ lab3/src/main/java/pl/edu/pwr/quizapp/lang/LocaleClassBundle_pl_PL.java
@@ -13,7 +13,7 @@ public class LocaleClassBundle_pl_PL extends ListResourceBundle {
                 {"qFindCityIn","Miasto | jest położone w:"},
                 {"aFindCityIn","{1,choice,0.0#Nie|1.0#Tak}, miasto {0} {1,choice,0.0#nie|1.0#} leży w {2}."},
                 {"qAlternativeNames","Miasto | inaczej nazywa się"},
-                {"aAlternativeNames","To {1,choice,0.0#Nie|1.0#} prawda, {1,choice,0.0#miasta|1.0#miasto} {0} {1,choice,0.0#nie|1.0#czasem} nazywa się {2}."},
+                {"aAlternativeNames","To {1,choice,0.0#nie|1.0#} prawda, {1,choice,0.0#miasta|1.0#miasto} {0} {1,choice,0.0#nie|1.0#czasem} nazywa się {2}."},
                 {"qCurrency","Kraj | używa waluty o kodzie"},
                 {"aCurrency","To {1,choice,0.0#nie|1.0#} prawda, {1,choice,0.0#waluty|1.0#walutę} o kodzie {2} {1,choice,0.0#nie|1.0#} używa się w {0}."},
         };
