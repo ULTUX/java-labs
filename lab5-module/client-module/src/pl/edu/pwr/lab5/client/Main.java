686fb90181a33cc2d04308523d8989c9a30caced
WNowak <bulion@zoho.com>
2022-04-06 18:40:15 +0200

    Add jars as dependencies

--- lab5-module/client-module/src/pl/edu/pwr/lab5/client/Main.java
+++ lab5-module/client-module/src/pl/edu/pwr/lab5/client/Main.java
@@ -1,17 +1,10 @@
 package pl.edu.pwr.lab5.client;
 
 import pl.edu.pwr.lab5.client.ui.MainFrame;
-import pl.edu.pwr.lab5.api.AnalysisService;
-
-import java.util.ServiceLoader;
 
 public class Main {
 
     public static void main(String[] args) {
-        ServiceLoader<AnalysisService> analysisServices = ServiceLoader.load(AnalysisService.class);
-        analysisServices.stream().forEach(analysisServiceProvider -> {
-            System.out.println(analysisServiceProvider.get().getName());
-        });
         new MainFrame();
     }
 }
