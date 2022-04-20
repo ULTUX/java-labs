08c6090add1d16b957b28bd782297707b9718bb1
WNowak <bulion@zoho.com>
2022-04-20 13:52:47 +0200

    Remove redundant code in Lab7Application

--- lab7/src/main/java/pl/edu/pwr/lab7/Lab7Application.java
+++ lab7/src/main/java/pl/edu/pwr/lab7/Lab7Application.java
@@ -1,14 +1,8 @@
 package pl.edu.pwr.lab7;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import pl.edu.pwr.lab7.jpa.event.EventService;
-import pl.edu.pwr.lab7.jpa.installment.InstallmentService;
-import pl.edu.pwr.lab7.jpa.payment.PaymentService;
-import pl.edu.pwr.lab7.jpa.person.PersonService;
 
 
 @SpringBootApplication
@@ -17,15 +11,6 @@ public class Lab7Application {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(Lab7Application.class).headless(false).run(args);
-    }
-
-    @Bean
-    CommandLineRunner commandLineRunner(EventService eventService, PersonService personService,
-                                        InstallmentService installmentService, PaymentService paymentService) {
-        return args -> {
-//            Thread.sleep(1000);
-//            new MainFrame(paymentService, eventService, personService, installmentService);
-        };
     }
 
 }
