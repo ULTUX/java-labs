eeeecc2c96932cc9d12cdf29db965ded4ba8d8b3
WNowak <bulion@zoho.com>
2022-04-27 00:57:53 +0200

    Add generated code from xml

---
+++ lab7/src/main/java/pl/edu/pwr/lab7/soap/ObjectFactory.java
@@ -0,0 +1,58 @@
+
+package pl.edu.pwr.lab7.soap;
+
+
+import pl.edu.pwr.lab7.jpa.person.Person;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the pl.edu.pwr.lab7.soap package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: pl.edu.pwr.lab7.soap
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link GetPerson }
+     * 
+     */
+    public GetPerson createGetPerson() {
+        return new GetPerson();
+    }
+
+    /**
+     * Create an instance of {@link GetResponse }
+     * 
+     */
+    public GetResponse createGetResponse() {
+        return new GetResponse();
+    }
+
+    /**
+     * Create an instance of {@link Person }
+     * 
+     */
+    public Person createPerson() {
+        return new Person();
+    }
+
+}
