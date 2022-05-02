dc110eb565194e00428c9d95d47761321cc96382
WNowak <bulion@zoho.com>
2022-05-02 20:56:16 +0200

    Update on lab9

--- lab9/gui/src/pl/edu/pwr/lab9/MainFrame.java
+++ lab9/gui/src/pl/edu/pwr/lab9/MainFrame.java
@@ -33,18 +33,19 @@ public class MainFrame extends JFrame {
         FileInputStream is = new FileInputStream("/home/ultux/IdeaProjects/wnowak_252700_java/lab9/keystore");
         KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
         ks.load(is, "password".toCharArray());
+        System.out.println(ks.getKey("main-key", "password".toCharArray()).getAlgorithm());
+
         Key key = ks.getKey("main-key", "password".toCharArray());
-        var secret = ks.getKey("mykey", "password".toCharArray());
+        var secret = ks.getKey("secret", "password".toCharArray());
         PublicKey pkey = null;
         if (key instanceof PrivateKey) {
             var cert = ks.getCertificate("main-key");
             pkey = cert.getPublicKey();
-            System.out.println(key);
         }
         try {
             var encDec = new FileEncrypterDecrypter();
-            encDec.rsaDecrypt("/home/ultux/IdeaProjects/wnowak_252700_java/lab9/gui/src/test.txt", secret);
-        } catch (NoSuchPaddingException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException e) {
+//            encDec.rsaDecrypt("/home/ultux/IdeaProjects/wnowak_252700_java/lab9/gui/src/test.txt", secret);
+        } catch (NoSuchPaddingException e) {
             throw new RuntimeException(e);
         }
         new MainFrame();
