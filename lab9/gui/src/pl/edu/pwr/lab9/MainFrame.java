9f7ce3878d539b493729fad3b25f8832960870d6
WNowak <bulion@zoho.com>
2022-05-03 17:25:44 +0200

    Develop GUI, finish transformation class

--- lab9/gui/src/pl/edu/pwr/lab9/MainFrame.java
+++ lab9/gui/src/pl/edu/pwr/lab9/MainFrame.java
@@ -2,23 +2,32 @@ package pl.edu.pwr.lab9;
 
 import pl.edu.pwr.lab9.lib.FileEncrypterDecrypter;
 
-import javax.crypto.*;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.*;
+import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 
 public class MainFrame extends JFrame {
     private JTextField inputFileField;
     private JButton inputButton;
-    private JComboBox algorithmSelector;
+    private JComboBox<String> algorithmSelector;
     private JButton encryptButton;
     private JButton decryptButton;
     private JPanel mainPanel;
-    private JComboBox comboBox1;
-    private JComboBox comboBox2;
+    private JComboBox<String> privateKeySelector;
+    private JComboBox<String> publicKeySelector;
+    private JTextField keyStoreLocField;
+    private JButton selectKeyStoreButton;
+    private JPasswordField passwordField1;
+    private JButton loadKeyStoreButton;
+
+    private KeyStore keyStore;
+
+    private File keyStoreLocation;
 
     public MainFrame() throws HeadlessException {
 
@@ -27,27 +36,90 @@ public class MainFrame extends JFrame {
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setVisible(true);
         this.pack();
+        selectKeyStoreButton.addActionListener(e -> selectKeyStore());
+        loadKeyStoreButton.addActionListener(e -> fetchKeys());
+        inputButton.addActionListener(e -> selectFile());
+        var algSelectModel = new DefaultComboBoxModel<String>();
+        algorithmSelector.setModel(algSelectModel);
+        algSelectModel.addElement("RSA");
+        algSelectModel.addElement("AES");
+
+    }
+
+    private void selectFile() {
+        var fileChooser = new JFileChooser();
+        fileChooser.setMultiSelectionEnabled(false);
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        var res = fileChooser.showDialog(this, "Select");
+        if (res == JFileChooser.APPROVE_OPTION) {
+            var file = fileChooser.getSelectedFile();
+            inputFileField.setText(file.toString());
+        }
+
+    }
+
+    private void selectKeyStore() {
+        var keyStoreLocChooser = new JFileChooser();
+        keyStoreLocChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        keyStoreLocChooser.setMultiSelectionEnabled(false);
+        var result = keyStoreLocChooser.showDialog(this, "Open");
+        if (result == JFileChooser.APPROVE_OPTION) {
+            keyStoreLocation = keyStoreLocChooser.getSelectedFile();
+            keyStoreLocField.setText(keyStoreLocation.toString());
+        }
+    }
+
+    private void fetchKeys() {
+        if (passwordField1.getPassword().length == 0) {
+            JOptionPane.showMessageDialog(this, "Empty password is not allowed", "Error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        try (var inputStream = new FileInputStream(keyStoreLocation)) {
+            keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            keyStore.load(inputStream, passwordField1.getPassword());
+
+            var privKeyModel = new DefaultComboBoxModel<String>();
+            var pubKeyModel = new DefaultComboBoxModel<String>();
+            privateKeySelector.setModel(privKeyModel);
+            publicKeySelector.setModel(pubKeyModel);
+            keyStore.aliases().asIterator().forEachRemaining(s -> {
+                privKeyModel.addElement(s);
+                Certificate cert = null;
+                try {
+                    cert = keyStore.getCertificate(s);
+                if (cert != null) {
+                    pubKeyModel.addElement(s);
+                }
+                } catch (KeyStoreException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
+
+        } catch (IOException | CertificateException | NoSuchAlgorithmException | KeyStoreException e) {
+            JOptionPane.showMessageDialog(this, "Could not load selected keystore.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
     }
 
     public static void main(String[] args) throws KeyStoreException, CertificateException, IOException, NoSuchAlgorithmException, UnrecoverableKeyException {
-        FileInputStream is = new FileInputStream("/home/ultux/IdeaProjects/wnowak_252700_java/lab9/keystore");
-        KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
-        ks.load(is, "password".toCharArray());
-        System.out.println(ks.getKey("main-key", "password".toCharArray()).getAlgorithm());
-
-        Key key = ks.getKey("main-key", "password".toCharArray());
-        var secret = ks.getKey("secret", "password".toCharArray());
-        PublicKey pkey = null;
-        if (key instanceof PrivateKey) {
-            var cert = ks.getCertificate("main-key");
-            pkey = cert.getPublicKey();
-        }
-        try {
-            var encDec = new FileEncrypterDecrypter();
-//            encDec.rsaDecrypt("/home/ultux/IdeaProjects/wnowak_252700_java/lab9/gui/src/test.txt", secret);
-        } catch (NoSuchPaddingException e) {
-            throw new RuntimeException(e);
-        }
+//        FileInputStream is = new FileInputStream("/home/ultux/IdeaProjects/wnowak_252700_java/lab9/keystore");
+//        KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+//        ks.load(is, "password".toCharArray());
+//        System.out.println(ks.getKey("main-key", "password".toCharArray()).getAlgorithm());
+//
+//        Key key = ks.getKey("main-key", "password".toCharArray());
+//        var secret = ks.getKey("secret", "password".toCharArray());
+//        PublicKey pkey = null;
+//        if (key instanceof PrivateKey) {
+//            var cert = ks.getCertificate("main-key");
+//            pkey = cert.getPublicKey();
+//        }
+//        try {
+//            var encDec = new FileEncrypterDecrypter();
+//            encDec.rsaDecrypt("/home/ultux/IdeaProjects/wnowak_252700_java/lab9/gui/src/test.txt", new KeyPair(pkey, (PrivateKey) key));
+//        } catch (NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException | InvalidKeyException e) {
+//            throw new RuntimeException(e);
+//        }
         new MainFrame();
     }
 }
--- lab9/lib/src/pl/edu/pwr/lab9/lib/FileEncrypterDecrypter.java
+++ lab9/lib/src/pl/edu/pwr/lab9/lib/FileEncrypterDecrypter.java
@@ -2,12 +2,16 @@ package pl.edu.pwr.lab9.lib;
 
 import javax.crypto.*;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Arrays;
+import java.util.Base64;
 
 public class FileEncrypterDecrypter {
 
@@ -15,22 +19,46 @@ public class FileEncrypterDecrypter {
     }
 
     public void rsaEncrypt(String fileName, KeyPair keyPair) throws NoSuchPaddingException, NoSuchAlgorithmException, IOException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-        byte[] fileBytes = Files.readAllBytes(FileSystems.getDefault().getPath(fileName));
-        Cipher encryptCipher = Cipher.getInstance("RSA");
-        encryptCipher.init(Cipher.ENCRYPT_MODE, keyPair.getPublic());
-        byte[] encryptedFileBytes = encryptCipher.doFinal(fileBytes);
-        try (FileOutputStream stream = new FileOutputStream(fileName)){
-            stream.write(encryptedFileBytes);
+        var oStream = new ByteArrayOutputStream();
+        try (var iStream = new FileInputStream(fileName)) {
+            var key = (RSAPublicKey) keyPair.getPublic();
+            Cipher encryptCipher = Cipher.getInstance("RSA");
+            encryptCipher.init(Cipher.ENCRYPT_MODE, keyPair.getPublic());
+            var maxEncrypt = (int) Math.floor(key.getModulus().bitLength() / 8.0) - 11;
+            while (iStream.available() != 0) {
+                byte[] encryptedFileBytes = new byte[maxEncrypt];
+                var read = iStream.read(encryptedFileBytes);
+                var encryptedTrueSize = Arrays.copyOf(encryptedFileBytes, read);
+                encryptedTrueSize = encryptCipher.doFinal(encryptedTrueSize);
+                oStream.write(encryptedTrueSize);
+            }
+            System.out.println(oStream.toString(StandardCharsets.UTF_8));
         }
+        try (FileOutputStream stream = new FileOutputStream(fileName, false)){
+            stream.write(Base64.getEncoder().encode(oStream.toByteArray()));
+        }
+
     }
 
     public void rsaDecrypt(String fileName, KeyPair keyPair) throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
-        byte[] encryptedFileBytes = Files.readAllBytes(FileSystems.getDefault().getPath(fileName));
-        Cipher decryptCipher = Cipher.getInstance("RSA");
-        decryptCipher.init(Cipher.DECRYPT_MODE, keyPair.getPublic());
-        byte[] decryptedFileBytes = decryptCipher.doFinal(encryptedFileBytes);
-        try (FileOutputStream stream = new FileOutputStream(fileName)) {
-            stream.write(decryptedFileBytes);
+        var oStream = new ByteArrayOutputStream();
+        try (var iStream = new FileInputStream(fileName)) {
+            var ist = new ByteArrayInputStream(Base64.getDecoder().decode(iStream.readAllBytes()));
+            var key = (RSAPublicKey) keyPair.getPublic();
+            Cipher decryptCipher = Cipher.getInstance("RSA");
+            decryptCipher.init(Cipher.DECRYPT_MODE, keyPair.getPrivate());
+            var maxDecrypt = (int) Math.floor(key.getModulus().bitLength()/8.0);
+            while (ist.available() != 0) {
+                byte[] encryptedFileBytes = new byte[maxDecrypt];
+                var read = ist.read(encryptedFileBytes);
+                byte[] encryptedTrueSize = Arrays.copyOf(encryptedFileBytes, read);
+                encryptedTrueSize = decryptCipher.doFinal(encryptedTrueSize);
+                oStream.write(encryptedTrueSize);
+            }
+        }
+
+        try (var foStream = new FileOutputStream(fileName, false)) {
+            foStream.write(oStream.toByteArray());
         }
     }
 
@@ -44,7 +72,7 @@ public class FileEncrypterDecrypter {
         }
     }
 
-    public void rsaDecrypt(String fileName, Key secret) throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+    public void aesDecrypt(String fileName, Key secret) throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
         byte[] encryptedFileBytes = Files.readAllBytes(FileSystems.getDefault().getPath(fileName));
         Cipher decryptCipher = Cipher.getInstance("AES");
         decryptCipher.init(Cipher.DECRYPT_MODE, secret);
